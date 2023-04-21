ef81bc6f33c21e4f1d368e39d930923e3f1345f2
SashBFa <bastienfajs@gmail.com>
2023-04-21 09:18:51 +0200

    Initialisation

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Seo } from '@/ui/component/seo';
+
+export default function Home() {
+  return (
+    <>
+      <Seo title="Coders Monkeys" description="Description..." />
+      <h1>Hello there</h1>
+    </>
+  );
+}
